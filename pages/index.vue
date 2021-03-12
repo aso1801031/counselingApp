1a07cacf587c989fb70d61a73c5f1e3656ad2d3b
aso1801031 <1801031@s.asojuku.ac.jp>
2021-03-13 02:10:55 +0900

    heroku

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,49 @@
 <template>
-  <div class="container">
-    <div>
-      <p>やあ! こんにちは</p>
-    </div>
-  </div>
+  <v-container fluid fill-height class="home">
+      
+      <v-card width=20% height=auto color="brown lighten-4">
+              
+        <v-card-text>
+            <v-btn class="homeBtn" large color="indigo" dark width=100% height=100px @click="login()">
+            <h2>ログイン</h2>
+            </v-btn>
+        </v-card-text>
+
+        <v-card-text>
+            <v-btn class="homeBtn" large color="indigo" dark width=100% height=100px @click="registration()">
+            <h2>新規登録</h2>
+            </v-btn>
+        </v-card-text>
+
+      </v-card>
+
+  </v-container>
 </template>
 
 <script>
-export default {
-  
-}
+  export default {
+      layout:"default1",
+      methods: {
+          login(){
+              this.$router.push('/login')
+          },
+          registration(){
+              this.$router.push('/registration')
+          },
+      }
+  }
 </script>
-
+  
 <style>
+.home {
+background: url("../static/images/background.jpg");
+background-size: cover;
+background-position: center top 38%;
+width: 100%;
+height: 100%;
+}
+.homeBtn {
+  padding: auto;
+  margin: auto;
+}
 </style>
