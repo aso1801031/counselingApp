4ae614db3bc935d80681698a7a01f6a08f1830a3
aso2001052 <2001052@s.asojuku.ac.jp>
2021-03-12 12:28:47 +0900

    home画面

--- pages/home.vue
+++ pages/home.vue
@@ -1,6 +1,33 @@
 <template>
-    <v-container>
-        <p>home画面だよ^^</p>
+    <v-container fluid fill-height class="container">
+
+        <!--<v-card class="mx-auto" max-width="900" align="center">
+            <v-card-text>
+                <v-btn class="mx-2" :loading="loading5" :disabled="loading5" fab dark x-large color="purple" @click="loader = 'loading5'">
+                    <v-icon dark>
+                        mdi-android
+                    </v-icon>
+                </v-btn>
+                
+                <p class="display-1 text--primary">
+                    相談室作成
+                </p>
+
+                <h5 class="text--primary" align="left"> タイトル</h5>
+                <v-col cols="12" sm="7">
+                    <v-text-field v-model="title" counter="25" label="タイトル"></v-text-field>
+                </v-col>
+
+                <h5 class="text--primary" align="left">カテゴリー</h5>
+                
+            </v-card-text>
+            
+            <v-card-actions>
+                <v-btn class="mx-auto" align="center" outlined depressed elevation="2" color="success" @click="addRoom()">
+                    作成
+                </v-btn>
+            </v-card-actions>
+        </v-card>-->
 
     </v-container>
 </template>
@@ -13,4 +40,11 @@
 </script>
     
 <style>
+.container {
+  background: url("../static/images/1990310_l.jpg");
+  background-size: cover;
+  background-position: center center;
+  width: 100%;
+  height: 100%;
+}
 </style>
